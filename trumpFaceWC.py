fc717d356658d70e1f261e1feb0b97835c0b49dc
Ethan Baker <ethanagbaker@gmail.com>
2016-06-21 10:37:03 -0400

    cleaner code

--- trumpFaceWC.py
+++ trumpFaceWC.py
@@ -1,21 +1,10 @@
-
-# coding: utf-8
-
-# In[3]:
-
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 
-
-# In[33]:
-
 insults = open('./trumpInsults.txt','r').read()
 trump_mask = np.array(Image.open('trump2.png'))
-
-
-# In[53]:
 
 bannedWords = ['said','new','will','york','many','the','total','never','united','states','failing','totally','news','bad','failed','people','senator',
               'party','one','state','always','absolutely','governor','make','read','anything','always','good','thing','really','job','lost','show','group',
@@ -24,18 +13,10 @@ bannedWords = ['said','new','will','york','many','the','total','never','united',
               'much','debate','debates','times','campaign','presidential','fox','clinton','hillary','bush','credibility','candidate','know','columnist','immigration',
               'another','ad','lied','chief','ted','record','newspaper','another','paid','journal','way','trump','got','life',
               'last','dead','street','great','clue','jeb']
+
 for word in bannedWords:
     STOPWORDS.add(word)
-
-
-# In[54]:
 
 wc = WordCloud(background_color="white", max_words=1500, mask=trump_mask, stopwords=STOPWORDS)
 wc.generate(insults)
 wc.to_file('trumpInsultWC.png')
-
-
-# In[ ]:
-
-
-
